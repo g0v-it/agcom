f0151a3fcacc3856ba9240e5b9982dbc6997ead0
napo <napo@fbk.eu>
2022-08-26 12:34:16 +0200

    update

---
+++ docs/js/interventi_politica.js
@@ -0,0 +1,48 @@
+var dom = document.getElementById('interventi_solo_politica');
+var myChartSoloPolitica = echarts.init(dom, null, {
+    renderer: 'canvas',
+    useDirtyRect: false
+});
+var optionSoloPolitica;
+
+myChartSoloPolitica.showLoading();
+$.get('/data/interventi_solo_politica.json', function(soloPoliticaData) {
+    myChartSoloPolitica.hideLoading();
+    optionSoloPolitica = {
+        title: {
+            text: soloPoliticaData.titolo
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+        legend: {},
+        grid: {
+            left: '10%',
+            right: '10%',
+            //bottom: '3%',
+            //containLabel: true
+        },
+        xAxis: {
+            type: 'value',
+            //boundaryGap: [0, 0.01]
+        },
+        yAxis: {
+            type: 'category',
+            data: soloPoliticaData.categorie
+        },
+        series: [{
+            name: soloPoliticaData,
+            type: 'bar',
+            data: soloPoliticaData.valori
+        }]
+    };
+
+
+    if (optionSoloPolitica && typeof optionSoloPolitica === 'object') {
+        myChartSoloPolitica.setOption(optionSoloPolitica);
+    }
+});
+window.addEventListener('resize', myChartSoloPolitica.resize);
